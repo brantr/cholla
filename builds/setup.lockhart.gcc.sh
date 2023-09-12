b601bf986287803bfe83b6a416549175365eff0a
bruno villasenor <bvillasen@gmail.com>
2023-09-12 10:46:36 -0700

    updated lockhart host files

--- builds/setup.lockhart.gcc.sh
+++ builds/setup.lockhart.gcc.sh
@@ -3,16 +3,12 @@
 #-- This script needs to be source-d in the terminal, e.g.
 #   source ./setup.lockhart.cce.sh
 
-module purge
-module load PrgEnv-gnu
-module load craype-x86-milan
-module load amd/5.3.0
-module load craype-accel-amd-gfx908
-export MPICH_GPU_SUPPORT_ENABLED=1
+module load cray-python
+module load rocm
+module load craype-accel-amd-gfx90a
+module load cray-hdf5 cray-fftw
 
 #-- GPU-aware MPI
 export MPICH_GPU_SUPPORT_ENABLED=1
-
-export LD_LIBRARY_PATH=${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
 
 export CHOLLA_ENVSET=1
